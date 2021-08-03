767ea5389fc645ccaabeff894363cfb9045275bb
ernestchu <b073040018@nsysu.edu.tw>
2021-08-03 10:46:29 +0800

    Initial Commit

---
+++ Vocabulary.com/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vocabulary.com
+//
+//  Created by Ernest Chu on 2021/8/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vocabulary.com/Vocabulary_comApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vocabulary_comApp.swift
+//  Vocabulary.com
+//
+//  Created by Ernest Chu on 2021/8/3.
+//
+
+import SwiftUI
+
+@main
+struct Vocabulary_comApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
